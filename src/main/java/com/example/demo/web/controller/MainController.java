e3a467b3d054fa583e587729fa4bce367e515ead
Артём <kostertem@yadnex.ru>
2020-10-29 01:27:35 +0400

    CORS

--- src/main/java/com/example/demo/web/controller/MainController.java
+++ src/main/java/com/example/demo/web/controller/MainController.java
@@ -2,15 +2,13 @@ package com.example.demo.web.controller;
 
 import com.example.demo.model.entities.Film;
 import com.example.demo.model.service.FilmService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin("http://localhost:4200/")
 public class MainController {
 
     private FilmService filmService;
