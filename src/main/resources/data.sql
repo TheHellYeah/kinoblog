04a3dc3f4a8a988298ad80292368407960483d0a
Артём <kostertem@yadnex.ru>
2020-11-19 06:36:23 +0400

    Next

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+
+insert into users(avatar, email, id, password, registration, username) values(null, 'l@l.ru', 1,
+'$2y$12$Fwwlsg5GX1oAQsMXJ/6Jz.0XvLybP9XhIJlOTNvthnvob/Kf9U1uK', null, 'Artem222');
+
+insert into roles(user_id, roles) values(1, 'ADMIN');
