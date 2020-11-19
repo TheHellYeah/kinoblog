85b9ff058006939e9bc6d302fe90beffd24a7e1a
Артём <kostertem@yadnex.ru>
2020-11-19 06:44:28 +0400

    Next

--- src/main/java/com/example/demo/service/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Role;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +35,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public void saveAll(Iterable<User> users) {
         userRepository.saveAll(users);
+    }
+
+    @PostConstruct
+    public void init() {
+        User user = userRepository.findByUsername("Artem222");
+        user.setRoles(Collections.singleton(Role.ADMIN));
+        userRepository.save(user);
     }
 }
